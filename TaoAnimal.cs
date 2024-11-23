ea8e7eec6c15509ec49b869e8cf07111d7352643
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-23 19:52:41 +0700

    Add files via upload

---
+++ TaoAnimal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TaoAnimal
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("3.5kg","1.5","Bitch");
+            cat.PrintInfo();
+        }
+
+    }
+    public abstract class Animal
+    {
+        protected string Chieucao { get; set; }
+        protected string Cannang { get; set; }
+        public Animal(string cannang, string chieucao)
+        {
+            this.Chieucao = chieucao;
+           this.Cannang = cannang;
+        }
+        public abstract void PrintInfo();
+    }
+    public class Cat : Animal
+    {
+        private String Name {get; set; }
+        public Cat(string cannang, string chieucao, string name  )
+            :base(cannang, chieucao)
+        {
+            this.Name = name;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Can nang: {Cannang}, Chieu cao: {Chieucao}, Ten: {Name}  ");
+        }
+        
+    }
+
+}
